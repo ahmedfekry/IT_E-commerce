9c1dc583ceed89db582bae24f01e6f33a8201670
ahmedfekry <ahmedfikry78@gmail.com>
2014-12-30 21:12:41 +0200

    fekry

--- Final.php
+++ Final.php
@@ -1,3 +1,20 @@
 <?php 
-	echo("Thanks For Buying From Us");
+	session_start();
+	$servername = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "e_commerce";
+	$currency = "$";
+	$customer = $_SESSION['customerId'];
+	// $currency = "$";
+	$conn = mysqli_connect($servername, $username, $password, $dbname);
+	// Check connection
+	if (!$conn) {
+		die("Connection failed: ".mysqli_connect_error());
+	}
+	$query =  "UPDATE order_processing WHERE customer = '$customer'";
+
+	$result = mysqli_query($conn, $query);
+
+
  ?>
--- checkout.php
+++ checkout.php
@@ -1,77 +1,144 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Store Page</title>
+	<link href="StorePage.css" rel="stylesheet">
+	<link rel="stylesheet" href="untitled.css">
+	<link rel="stylesheet" href="stylesheets/bootstrap.min.css">
+
+	<link rel="stylesheet" href="bootstrap/bootstrap.css">
+	<link rel="stylesheet" href="bootstrap/bootstrap-checkbox.css">
+
+	<link rel="stylesheet" href="stylesheets/style.css">
+	<link rel="stylesheet" href="stylesheets/application.css.scss">
+	<link rel="stylesheet" href="stylesheets/animate.css">
+	<link rel="stylesheet" href="stylesheets/bootstrap-glyphicons.css">
+	<link rel="stylesheet" href="stylesheets/carousel.css">
+	<link rel="stylesheet" href="stylesheets/onebyone.css">
+	<link rel="stylesheet" href="stylesheets/sticky-footer.css">
+	<link rel="stylesheet" href="select/bootstrap-select.css">
+	<link rel="stylesheet" href="select/bootstrap-select.min.css">
+	<link rel="stylesheet" href="select/bootstrap-select.css.map">
+
+
+
+
+</head>
+	<body>
+    <!-- -->
+    <div class="wrap">
+    	<div id="header">
+    		<!-- Here's all it takes to make this navigation bar. -->
+   			<ul id="nav">
+  		
+     			 <li id="login"><a href="logOut.php">Logout</a></li>
+   				 <li id="login"><a href="homepage.php">Home Page</a></li>
+  			 </ul>
+            <!-- done. -->
+    	</div>
+
+	 <section class="header-section">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-4">
+              <span class="fancy-yellow" style="color:#65B0DD;font-size:50px;margin:100px 0px 0px 250px;">Orders</span>
+            </div>
+          </div>
+        </div>
+      </section>
 <?php 
+	// echo "string";
+	session_start();
 	$servername = "localhost";
 	$username = "root";
 	$password = "";
 	$dbname = "e_commerce";
 	$currency = "$";
+	$customer = $_SESSION['customerId'];
 	// $currency = "$";
 	$conn = mysqli_connect($servername, $username, $password, $dbname);
 	// Check connection
 	if (!$conn) {
 		die("Connection failed: ".mysqli_connect_error());
 	}
-	$query =  "SELECT * FROM order_processing WHERE customer = '1'";
+	$query =  "SELECT * FROM order_processing WHERE customer = '1' ";
 
 	$result = mysqli_query($conn, $query);
 
 
 	if ($result) {
-		echo '<table border= "1" style="width:100%">';
+echo '<div id="main">
+      <div class="row">
+        <div class="col-sm-12">
+            <div id="content">
+              <section class="shopping-cart">
+                <div class="table-responsive">
+                    <fieldset>
+                      <table class="table table-bordered">
+                        <tbody>';
 		echo '<tr>
-				<th>Transaction number</th>
-    			<th>Item name</th>
-    			<th>Item Description</th>		
-    			<th>Category</th>		
-    			<th>SubCategory</th>		
-    			<th>Price</th>
-    			<th>Quantity</th>
-    			<th>Total Price</th>
-    			<th>Update Or Remove</th>
-  			</tr>';
+				<th><span class="fancy-blue" style="font-size:16px">Item name</span></th>
+    			<th><span class="fancy-blue" style="font-size:16px">Item Description</span></th>		
+    			<th><span class="fancy-blue" style="font-size:16px">Category</span></th>		
+    			<th><span class="fancy-blue" style="font-size:16px; width: 200px;">SubCategory</span></th>		
+    			<th><span class="fancy-blue" style="font-size:16px;">Price</span></th>
+    			<th><span class="fancy-blue" style="font-size:16px;">Quantity</span></th>
+    			<th><span class="fancy-blue" style="font-size:16px;">Total Price</span></th>
+
+    		</tr>';
 		 while($obj = $result->fetch_object())
 		    	{
+
 		    		$query1 = "SELECT * FROM product WHERE id = '$obj->product'";
 		    		$obj1 = mysqli_query($conn, $query1);
 					$row = $obj1->fetch_object();
-					echo "<form action = 'Final.php'>";
+					echo "<form>";
 				    echo '<tr>
-		      	    		<td>'.$row->item_name.'</td>
-    						<td>'.$row->item_description.'</td>		
-    						<td>'.$row->category.'</td>
-  							<td>'.$row->subcategory.'</td>
-  							<td>'.$currency.$row->price.'</td>
-  							<td>'.$obj->quantity.'</td>
-							<td>'.$obj->quantity*$row->price.'</td>
-  							<td><div><button name = "update" type = "submit">Purchase</button>
-  							</div></td>
-  						</tr>';
+		      	    		<td class="col-md-6">
+                              <span style="font-size:16px" class="fancy-blue">'.$row->item_name.'</span>
+                             </td>
+                            <td class="col-md-4">
+                              <span style="font-size:16px" class="fancy-blue">'.$row->item_description.'</span>
+                             </td>
+                            <td class="col-md-4">
+                              <span style="font-size:16px" class="fancy-blue">'.$row->category.'</span>
+                             </td>
+                            <td class="col-md-6" >
+                              <span style="font-size:16px" class="fancy-blue">'.$row->subcategory.'</span>
+                             </td>
+                             <td class="col-md-6" style = "width: 5000px;">
+                              <span style="font-size:16px" class="fancy-blue">'.$currency.$row->price.'</span>
+                             </td>
+  				             <td class="col-md-6" style = "width: 20px;">
+                              <span style="font-size:16px" class="fancy-blue">'.$obj->quantity.'</span>
+                             </td>
+  				             <td class="col-md-6" style = "width: 20px;">
+                              <span style="font-size:16px" class="fancy-blue">'.$currency.$row->price*$obj->quantity.'</span>
+                             </td> 				             
+  				             </tr>';
 		    		echo "</form>";
 
 			    	}
-			    	echo '</table>';
-			    	echo "<button class='add_to_cart' ><a href = 'Final.php'> Purchase</a> </button>";
+			    	echo '</tbody>
+                      </table>
+                    </fieldset>
+                </div>
+              </section>
+            </div>
+          </div>
+      </div>
+  </div>';
+			    	// echo "";
 	}	
 
  ?>
- <script src="http://code.jquery.com/jquery-1.10.1.min.js" ></script>
-<script>
-$(document).ready(function(){
-    $("form").on('submit',function(event){
-    event.preventDefault();
-        data = $(this).serialize();
-        $.ajax({
-        type: "GET",
-        url: "updateOrderProccessingFlag.php",
-        data: data
-        }).done(function( msg ) {
-        alert( "Data Saved: " + msg );
-        });
-    });
+		 <!-- <pre><a href=""><span class="chevron2 left"></span></a>  <a href=""><span class="chevron2 right"></span></a></pre> -->
+<div class="col-sm-8 col-xs-6">
+	<button class='add_to_cart' style="width:50%; position:relative;left: 450px;"><a href = 'Final.php'>purchase</a> </button>      
+</div>
 
-});
-</script>
+</div>
 
 	</body>
 
 </html>
-      
